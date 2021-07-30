91c38672f22cabef1b48750b7f17462583047094
tiagosantini <tiagosantiniathayde@outlook.com>
2021-07-30 14:33:42 -0300

    Adicionei o projeto inicial

---
+++ CalculadoraTabajara.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTabajara.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Calculadora Tabajara\n");
+
+                Console.WriteLine("Digite a operação que deseja realizar: ");
+
+                Console.WriteLine("Digite 1 para realizar cálculos de adição");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1": throw new NotImplementedException(); break;
+
+                    default: break;
+                }
+            }
+        }
+    }
+}
